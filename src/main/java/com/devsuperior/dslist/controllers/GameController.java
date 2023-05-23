6dd030116a06d74d8f7fbbd7b10c8d46be38245a
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-05-22 22:31:50 -0300

    Get lists

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -19,15 +19,15 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 	
-	@GetMapping
-	public List<GameMinDTO> findAll(){
-		List<GameMinDTO> result = gameService.findAll();
-		return result;
-	}
-	
 	@GetMapping(value = "{id}")
 	public GameDTO findById(@PathVariable Long id){
 		GameDTO result = gameService.findById(id);
+		return result;
+	}
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> result = gameService.findAll();
 		return result;
 	}
 
